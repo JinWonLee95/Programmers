653df9619398a9a7dc87e5713c75ba6efa0bf7cd
JinWon Lee <dlwlsdnjs456@naver.com>
2020-09-07 01:35:57 +0900

    new quest

---
+++ src/Kakao_2019/Open_Chat.java
@@ -0,0 +1,10 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42888
+
+package Kakao_2019;
+
+public class Open_Chat {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
