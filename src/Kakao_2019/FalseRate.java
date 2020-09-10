50d4cdfcf43c6eb7430006eecf985472162a6f3d
JinWon Lee <dlwlsdnjs456@naver.com>
2020-09-10 23:45:25 +0900

    false rate solving

---
+++ src/Kakao_2019/FalseRate.java
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42889
+
+package Kakao_2019;
+
+public class FalseRate {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        int people = 0;
+        return answer;
+    }
+}
