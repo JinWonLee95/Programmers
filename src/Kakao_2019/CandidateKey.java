0efc2d92a905c1d20fb29a647f66c043445c16a0
JinWon Lee <dlwlsdnjs456@naver.com>
2020-09-11 00:41:00 +0900

    Candidate Key Solving

---
+++ src/Kakao_2019/CandidateKey.java
@@ -0,0 +1,10 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42890
+
+package Kakao_2019;
+
+public class CandidateKey {
+    public int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+}
