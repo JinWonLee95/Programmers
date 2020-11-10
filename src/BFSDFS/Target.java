afcd6e675204647f43b8d7263977b70e2c1285b5
JinWon Lee <dlwlsdnjs456@naver.com>
2020-11-10 19:28:44 +0900

    BFS done

--- src/BFSDFS/Target.java
+++ src/BFSDFS/Target.java
@@ -6,13 +6,23 @@ public class Target {
     public int solution(int[] numbers, int target) {
         int answer = 0;
 
+        answer = DFS(numbers, target, 0,0);
         return answer;
+    }
+
+    public int DFS(int[] numbers, int target, int index, int num) {
+        if(index == numbers.length) {
+            return num == target ? 1 : 0;
+        } else {
+            return DFS(numbers, target, index + 1, num + numbers[index])
+                    + DFS(numbers, target, index + 1, num - numbers[index]);
+        }
     }
 
     public static void main(String[] args) {
         int[] numbers = {1, 1, 1, 1, 1};
         int target = 3;
 
-        new Target().solution(numbers, target);
+        System.out.println(new Target().solution(numbers, target));
     }
 }
